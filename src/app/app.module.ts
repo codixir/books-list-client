e2f346c9e97cb2e5d2ab086f36a7c03763af738a
codixir <codixir@gmail.com>
2018-10-05 06:06:51 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from "@angular/common/http";
+
+import { HttpModule } from '@angular/http';
+
+import {ReactiveFormsModule} from "@angular/forms";
+
 import { AppComponent } from './app.component';
+import { ListBooksComponent } from './list-books/list-books.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { routing } from "./app.routing"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBooksComponent,
+    AddBookComponent,
+    EditBookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
